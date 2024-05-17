51a4568c8907872ea1f527aad318dc4a01d27452
Samanta1984 <samantaramos1984@hotmail.com>
2024-05-17 21:34:07 +0200

    proyecto ISTQB DAR BIENVENIDA Y VER RESPUESTA

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.write("HOLA MUNDO");
+function mostrarRespuesta() {
+    let nombre = document.getElementById("nombre1").value;
+    let primerApellido = document.getElementById("apellido1").value;
+    let segundoApellido = document.getElementById("apellido2").value;
+    let mensaje = "Gracias " + nombre + " " + primerApellido + " " + segundoApellido + " por facilitarnos sus datos. En breve le redirigiremos al examen.";
+    
+    document.getElementById("respuesta1").textContent = mensaje;//sacado de chatgpt ya que no me imprimia el mensaje//
+    
+}
+
+
+
