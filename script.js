c725087227034c693b6e7cd064d3b78cb738ed61
Samanta1984 <samantaramos1984@hotmail.com>
2024-05-18 13:24:00 +0200

    proyeto ISTQB
    DAR BIENVENIDA Y VER RESPUESTA - 5. Funciones y Eventos
    
    CORREGIR PREGUNTA Y VER PUNTUACIÓN - 6. Condicionales

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ function mostrarRespuesta() {
     let nombre = document.getElementById("nombre1").value;
     let primerApellido = document.getElementById("apellido1").value;
     let segundoApellido = document.getElementById("apellido2").value;
-    let mensaje = "Gracias " + nombre + " " + primerApellido + " " + segundoApellido + " por facilitarnos sus datos. En breve le redirigiremos al examen.";
+    let mensaje = "Thank you " + nombre + " " + primerApellido + " " + segundoApellido + "for providing us with your information. We will redirect you to the exam shortly.";
     
-    document.getElementById("respuesta1").textContent = mensaje;//sacado de chatgpt ya que no me imprimia el mensaje//
+    document.getElementById("respuesta1").textContent = mensaje;
     
 }
 
 
+
+
+function questionCorrect() {
+    let respuesta = document.getElementById("respuestas").value;
+    let mensaje = "";
+
+    if (respuesta === "b") {
+        mensaje = "The answer is correct, you get two points.";
+    } else if (respuesta === "a") {
+        mensaje = "The answer is correct, you get two points.";
+    } else if (respuesta === "c") {
+        mensaje = "The answer is incorrect, you get zero points.";
+    } else if (respuesta === "d") {
+        mensaje = "The answer is incorrect, you get zero points.";
+    } else {
+        mensaje = "If no answer is selected, you get zero points.";
+    }
+
+    document.getElementById("resultadoPregunta").textContent = mensaje;
+}
 
