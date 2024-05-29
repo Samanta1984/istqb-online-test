b714361a54c9c1d78c9ef17bb76b0491ea6d1ce5
Samanta1984 <samantaramos1984@hotmail.com>
2024-05-29 16:55:54 +0200

    Correct Question
    tercera parte finalizada

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let respuestaCorrecta = ["b","a","c","a","c","c","a","b","c","a"];
+let respuestaCorrecta = ["b", "a", "c", "a", "c", "c", "a", "b", "c", "a"];
 
 // Primera : Welcome Student
 
 function mostrarBienvenida() {
     let nombre = document.getElementById("nombre1").value;
-    let mensaje = " Welcome " + nombre ;
+    let mensaje = " Welcome " + nombre;
     document.getElementById("respuestaBienvenida").innerText = mensaje;
-    
+
 }
 
 
@@ -17,34 +17,29 @@ function verRespuesta(numeroPregunta) {
     let respuesta = respuestaCorrecta[numeroPregunta];
     let mensaje = " The correct answer is " + respuesta.toUpperCase();
 
-    document.getElementById("resultadoVerRespuesta"+numeroPregunta).innerText = mensaje;
+    document.getElementById("resultadoVerRespuesta" + numeroPregunta).innerText = mensaje;
 }
 
 
 
-// Tercera: Correct Question
-function corregir() {
-    let respuestaSeleccionada = document.getElementById("respuestas0").value;
+//Tercera: Correct Question
+
+function corregir(numeroPregunta) {
+    let respuestaSeleccionada = document.getElementById("respuesta" + numeroPregunta).value;
     let mensaje = "";
     let puntuacion = 0;
     let respuesta = "";
 
-    if (respuestaSeleccionada === respuestaCorrecta) {
+    if (respuestaSeleccionada === respuestaCorrecta[numeroPregunta]) {
         puntuacion = 2;
         respuesta = "correct";
-    }
-    else if (respuestaSeleccionada === ""){
+    } else if (respuestaSeleccionada === "") {
         puntuacion = 0;
         respuesta = "incorrect";
-
-    }
-    else {
+    } else {
         puntuacion = -1;
         respuesta = "incorrect";
-        
     }
-
     mensaje = " The question is " + respuesta + " you recive " + puntuacion;
-
-    document.getElementById("resultadoCorregir0").innerText = mensaje;
+    document.getElementById("resultadoCorregir" + numeroPregunta).innerText = mensaje;
 }
